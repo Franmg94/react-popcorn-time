71e98cc8133bcef35bd5eff6b0258bcad0db8779
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-11-09 12:40:13 +0100

    (lift state up) display number of movies in Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import movies from "./data/movies.json";
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
@@ -5,10 +7,23 @@ import Main from './components/Main'
 
 function App() {
 
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+  const deleteMovie = (movieId) => {
+    const newList = moviesToDisplay.filter((movieDetails) => {
+      return movieDetails.id !== movieId;
+    });
+
+    // moviesToDisplay = newList; // NEVER UPDATE STATE DIRECTLY !!
+    // moviesToDisplay.pop(); // NEVER UPDATE STATE DIRECTLY !!
+    setMoviesToDisplay(newList);
+  }
+
+
   return (
     <>
-      <Header />
-      <Main />
+      <Header numberOfMovies={moviesToDisplay.length} />
+      <Main moviesArr={moviesToDisplay} callbackToDelete={deleteMovie}  />
       <Footer />
     </>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,40 +1,16 @@
-import { useState } from "react";
-import movies from "../data/movies.json";
 import Movie from "./Movie";
 
-function Main(){
-
-    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
-
-    const deleteMovie = (movieId) => {
-        const newList = moviesToDisplay.filter( (movieDetails) => {
-            return movieDetails.id !== movieId;
-        });
-        
-        // moviesToDisplay = newList; // NEVER UPDATE STATE DIRECTLY !!
-        // moviesToDisplay.pop(); // NEVER UPDATE STATE DIRECTLY !!
-        setMoviesToDisplay(newList);
-    }
-
-    let message = "";
-    if(moviesToDisplay.length){
-        message = <h1>Number of movies: {moviesToDisplay.length}</h1>
-    } else {
-        message = <h1>Sorry, no movies to display</h1>
-    }
+function Main(props){
 
     return (
         <>
 
-            {message}
-
-            {moviesToDisplay.map(function(movieObj){
+            {props.moviesArr.map(function(movieObj){
                 return (
                     <Movie 
                         key={movieObj.id}
                         movieDetails={movieObj}
-                        title="hello"
-                        callbackToDelete={deleteMovie}
+                        callbackToDelete={props.callbackToDelete}
                         />
                 )
             })}
